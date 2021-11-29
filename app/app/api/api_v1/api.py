473c9579d5dd3af93ed756b2503b38c44d57b0b6
tenzin <ten13zin@gmail.com>
2021-11-29 17:19:48 +0530

    fix(work): add works api endpoints

--- app/app/api/api_v1/api.py
+++ app/app/api/api_v1/api.py
@@ -7,14 +7,20 @@ from app.api.api_v1.endpoints import (
     pedurma,
     proofread,
     users,
+    works,
 )
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["Authentication"])
-api_router.include_router(users.router, prefix="/users", tags=["Users"])
+
+api_router.include_router(works.router, prefix="/works", tags=["Works"])
 api_router.include_router(pechas.router, prefix="/pechas", tags=["Pechas"])
+
 api_router.include_router(pedurma.router, prefix="/pedurma", tags=["Pedurma"])
+
 api_router.include_router(proofread.router, prefix="/proofread", tags=["Proofread"])
 api_router.include_router(
     diffproofread.router, prefix="/diffproofread", tags=["DiffProofread"]
 )
+
+api_router.include_router(login.router, tags=["Authentication"])
+api_router.include_router(users.router, prefix="/users", tags=["Users"])
