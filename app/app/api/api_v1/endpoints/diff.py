9ebe8fa19f9c034fd461c6a8ebe48489eff44412
tenzin <ten13zin@gmail.com>
2022-01-03 12:02:38 +0530

    fixdiff): endpoint

--- app/app/api/api_v1/endpoints/diff.py
+++ app/app/api/api_v1/endpoints/diff.py
@@ -8,7 +8,7 @@ from app.services.diff import Diff
 router = APIRouter()
 
 
-@router.post("/", response_model=List[list])
+@router.post("", response_model=List[list])
 def find_diffs(input: DiffInput):
     diff = Diff(input.textA, input.textB)
     return diff.compute()
